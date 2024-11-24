d183967a2eea102d744ab70e81660ffa6f94e1e0
Josiah Miggiani <jmiggian@andrew.cmu.edu>
2024-11-24 11:34:07 -0500

    Cleaned up python script

--- gcode/serial_comms_gcode.py
+++ gcode/serial_comms_gcode.py
@@ -4,33 +4,28 @@ import time
 # Taken from https://projecthub.arduino.cc/ansh2919/serial-communication-between-python-and-arduino-663756
 # arduino = serial.Serial(port='COM7', baudrate= 115200, timeout=.1)
 
-def write_read(x): 
-    arduino.write(bytes(x, 'utf-8')) 
-    time.sleep(0.05) 
-    data = arduino.readline() 
-    return data
+PAUSE_1S = "G4 P1"
+GOTO_ZERO = "G1 X0 Y0"
 
-def gcode_goto(x: float, y: float):
-    # bounds checking
+# send XY coordinate to go to
+def gcode_goto(s, x: float, y: float):
+    # bounds check
     if not (-10 <= x <= 10): return -1
     elif not (-10 <= y <= 10): return -1
 
     gcode = 'G1 X' + str(x) + ' Y' + str(y) + '\n'
-    write_read(gcode)
-    return
+    s.write(bytes(gcode + '\n', 'utf-8'))
+    grbl_out = s.readline() # Wait for grbl response with carriage return
+    print(' : ' + str(grbl_out.strip()))
 
+# send raw gcode command
 def gcode_send(s, command: str):
     s.write(bytes(command + '\n', 'utf-8'))
     grbl_out = s.readline() # Wait for grbl response with carriage return
     print(' : ' + str(grbl_out.strip()))
 
-
-PAUSE_1S = "G4 P1"
-GOTO_ZERO = "G1 X0 Y0"
-def main():
-    # Open grbl serial port
-    s = serial.Serial('COM7',115200)
-
+# initialize grbl connection after opening serial comms
+def grbl_init(s):
     # Wake up grbl
     print("Waking up grbl")
     s.write(bytes("\r\n\r\n", 'utf-8'))
@@ -49,18 +44,31 @@ def main():
     gcode_send(s, "F2000") # Set feed rate. for some reason, has to be low right after homing
     gcode_send(s, GOTO_ZERO) # Go to zero zero
     gcode_send(s, PAUSE_1S)
-
     gcode_send(s, "F3750") # Set feed rate for normal operation
-    print("Now accepting user input. Enter q to exit")
-    while True: # user input loop
-        cmd = input("\nEnter command to send to grbl: ")
-        if cmd == "q": break
+    return
 
-        gcode_send(s, cmd)
+
+def main():
+    # Open grbl serial port
+    s = serial.Serial('COM7',115200)
+
+    # initialize grbl connection
+    grbl_init(s)
+
+    print("Now accepting user input. Enter q to exit")
+    print("Shortcut to send coordinates: c X Y")
+    print("Otherwise, send raw GCODE commands")
+    while True: # user input loop
+        cmd = input("\nEnter command to send to grbl: ").strip()
+        if cmd == "q": break
+        elif cmd[0].lower() == "c": # goto X Y
+            c, x, y = cmd.split(" ")
+            gcode_goto(s, int(x), int(y))
+        else: # send RAW gcode command
+            gcode_send(s, cmd)
         gcode_send(s, PAUSE_1S)
         gcode_send(s, GOTO_ZERO)
 
-    # input("  Press <Enter> to exit and disable grbl.") 
     # Close file and serial port
     s.close()
 
